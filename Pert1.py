36c1a32c3de3c8c80f0c69eafd93da1e39afba63
Your Name <you@example.com>
2021-10-25 15:02:39 +0700

    Simpan semua file pert-1

---
+++ Pert1.py
@@ -0,0 +1,15 @@
+# harus cek install python dulu
+print ("Hello, World!")
+
+# membuat variables
+x = 5 # tipe data integer
+y = "Hello, World!"
+
+print (x)
+
+#tipe data : String (kalimat), Integer (angka wo decimals), Float (decimals), Boolean (true false), 
+# kalkulasi aritmatika dikenali di python
+
+int (x)
+float (x)
+str (x)
