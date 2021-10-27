273ecf662c2c635e196a7a417f6e1ef623bf3ee4
Your Name <you@example.com>
2021-10-27 13:26:20 +0700

    catatan pert2

---
+++ Pert-2.py
@@ -0,0 +1,24 @@
+# name = input("Enter name: ")
+# print("Your name is ", name)
+
+my_string = "Hello, World!"
+print(len(my_string))
+print(my_string[0:6])
+
+my_string = my_string.replace ("World", "love")
+
+print(my_string)
+a = "Rich"
+b = "Happy"
+c = a + b
+print (c)
+
+nama = "kiway"
+alamat = "Bogor"
+my_profile = "Nama saya {} dan saya tinggal di {}.". format (nama, alamat)
+
+print(my_profile)
+
+x = "Indonesia AI"
+print(len(x))
+print(x[2:4])
